98abaafaf664c25a5113124b4049984f9f24320b
Ubiratan Pires Alberton <u.alberton@gmail.com>
2009-09-05 19:51:09 -0300

    Font changes

--- bira.el
+++ bira.el
@@ -1,19 +1,12 @@
 ;;; elisp libraries I run from source checkouts:
 
-(add-to-list 'load-path "/home/phil/src/elisp/clojure-mode")
-
-;;; Random stuff
-
-;; If we don't have XFT, let's at least pick a decent default.
-(if (< emacs-major-version 23)
-    (ignore-errors
-      (set-default-font "-xos4-terminus-medium-r-normal--16-160-72-72-c-80-iso8859-1")))
+;(add-to-list 'load-path "/home/phil/src/elisp/clojure-mode")
 
 (eval-after-load 'ruby-mode
   '(add-hook 'ruby-mode-hook 'esk-paredit-nonlisp))
 
 ;; unfortunately some codebases use tabs. =(
-(set-default 'tab-width 4)
+(set-default 'tab-width 2)
 (set-default 'c-basic-offset 2)
 
 (defun eshell/rm (&rest args)
